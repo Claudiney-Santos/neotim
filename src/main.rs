2af8ed7c0a3065ba800618c061dd9e65251b4901
Tiago Panizio Gottardo <tiago.panizio@hotmail.com>
2026-06-19 04:16:43 -0300

    feat: implement insert mode

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::{
     fs,
     io::{Read, stdin},
-    process::Command,
+    process::{Command, exit},
 };
 
 macro_rules! prin {
@@ -12,67 +12,107 @@ macro_rules! prin {
 }
 
 const CLEAR_SCREEN: &str = "\x1b[2J\x1b[H";
-// const HIDE_CURSOR: &str = "\x1b[?25l";
-// const SHOW_CURSOR: &str = "\x1b[?25h";
-// const UP: &str = "\x1b[1A";
-// const DOWN: &str = "\x1b[1B";
-// const RIGHT: &str = "\x1b[1C";
-// const LEFT: &str = "\x1b[1D";
 
-// enum Mode {
-//     NORMAL,
-//     VISUAL,
-//     INSERT,
-// }
+#[derive(PartialEq)]
+enum Mode {
+    Normal,
+    // VISUAL,
+    Insert,
+}
 
 struct Ctx {
-    // mode: Mode,
-    content: String,
-    size: i32,
-    x: i32,
-    y: i32,
+    mode: Mode,
+    content: Vec<String>,
+    x: usize,
+    y: usize,
 }
 
 impl Ctx {
     fn new() -> Result<Self, anyhow::Error> {
-        let content = fs::read_to_string("./README.md")?.replace("\n", "\r\n");
-        let size = content.matches("\n").count() as i32;
+        let content = fs::read_to_string("./README.md")?
+            .split("\n")
+            .map(|l| format!("{l}\r\n"))
+            .collect::<Vec<String>>();
 
         Ok(Self {
+            mode: Mode::Normal,
             content,
-            size,
             x: 0,
             y: 0,
         })
     }
 
-    fn up(&mut self, n: i32) {
-        self.y -= n;
-        prin!("\x1b[{n}A")
+    fn up(&mut self, n: usize) {
+        if self.y as i32 - n as i32 >= 0 {
+            self.y -= n;
+            prin!("\x1b[{n}A")
+        }
     }
 
-    fn down(&mut self, n: i32) {
-        if self.y + n < self.size {
+    fn down(&mut self, n: usize) {
+        if self.y + n < self.content.len() {
             self.y += n;
             prin!("\x1b[{n}B")
         }
     }
 
-    fn right(&mut self, n: i32) {
+    fn right(&mut self, n: usize) {
         self.x += n;
         prin!("\x1b[{n}C")
     }
 
-    fn left(&mut self, n: i32) {
-        self.x -= n;
-        prin!("\x1b[{n}D")
+    fn left(&mut self, n: usize) {
+        if self.x as i32 - n as i32 >= 0 {
+            self.x -= n;
+            prin!("\x1b[{n}D")
+        }
     }
 
-    fn go_to(&mut self, x: i32, y: i32) {
+    fn go_to(&mut self, x: usize, y: usize) {
         self.x = x;
         self.y = y;
         prin!("\x1b[{y};{x}H");
     }
+
+    fn thin_cursor(&self) {
+        prin!("\x1b[6 q");
+    }
+
+    fn block_cursor(&self) {
+        prin!("\x1b[2 q");
+    }
+
+    fn print_all(&self) {
+        for line in self.content.iter() {
+            prin!("{line}");
+        }
+    }
+}
+
+fn exec_command(_: &Ctx) -> Result<(), anyhow::Error> {
+    let mut result = String::from(":");
+
+    loop {
+        let mut key = [0; 1];
+        stdin().read_exact(&mut key)?;
+
+        match key[0] as char {
+            '\r' | '\n' => break,
+            c => result.push(c),
+        }
+    }
+
+    if result.contains("w") {
+        // #TODO: Save content
+    }
+
+    if result.contains("q") {
+        Command::new("stty").arg("sane").status()?;
+        print!("{CLEAR_SCREEN}");
+        exit(0);
+    }
+
+    Ok(())
 }
 
 fn main() -> Result<(), anyhow::Error> {
@@ -81,25 +121,46 @@ fn main() -> Result<(), anyhow::Error> {
     Command::new("stty").args(["raw", "-echo"]).status()?;
 
     print!("{CLEAR_SCREEN}");
-    print!("{}", ctx.content);
-    prin!("{}", ctx.size);
-
+    ctx.print_all();
     ctx.go_to(0, 0);
+
     loop {
         let mut key = [0; 1];
         stdin().read_exact(&mut key)?;
 
-        match key[0] as char {
-            'q' => break,
-            'h' => ctx.left(1),
-            'j' => ctx.down(1),
-            'k' => ctx.up(1),
-            'l' => ctx.right(1),
-            _ => {}
+        if ctx.mode == Mode::Normal {
+            match key[0] as char {
+                'q' => break,
+                'h' => ctx.left(1),
+                'j' => ctx.down(1),
+                'k' => ctx.up(1),
+                'l' => ctx.right(1),
+                'i' => {
+                    ctx.mode = Mode::Insert;
+                    ctx.thin_cursor();
+                }
+                ':' => exec_command(&ctx)?,
+                _ => {}
+            }
+        } else {
+            match key[0] as char {
+                '\x1b' => {
+                    ctx.mode = Mode::Normal;
+                    ctx.block_cursor();
+                }
+                c => {
+                    prin!("\x1b[1@{}", c); // Write aside right letters
+                    ctx.content[ctx.y].insert(ctx.x, c);
+                    ctx.x += 1;
+                }
+            }
         }
     }
 
     Command::new("stty").arg("sane").status()?;
+    print!("{CLEAR_SCREEN}");
 
     Ok(())
 }
+
+// prin!("\x1b[1L"); break line
